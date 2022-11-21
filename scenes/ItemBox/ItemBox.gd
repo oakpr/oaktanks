241ce2ff61f82066384a363c61c1eba218b20a04
Graham B <graham@brunning.net>
2022-11-20 18:12:23 -0600

    ItemBox fixes
    ItemBox handles Item nodes now

--- scenes/ItemBox/ItemBox.gd
+++ scenes/ItemBox/ItemBox.gd
@@ -4,13 +4,10 @@ extends KinematicBody
 onready var body: MeshInstance = $body
 onready var mesh: PrismMesh = body.get("mesh")
 var time = 0
-var stash = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# replace this with child nodes that inherit Item 
-	var cool_sword = "cool_sword"
-	stash.push_front(cool_sword)
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,11 +19,9 @@ func _process(delta):
 # called when another Area enters this Area
 func _on_Box_area_entered(area: Area):
 	if area.name == "PickupZone":
-		# replace this with:
-		#for loot in get_childen():
-		for loot in stash:
-			if loot is Item:
-				area.get_parent().add_item(loot)
-			else:
-				print(loot + " moved to player")
+		for loot in get_children():
+			if !(loot is Item):
+				continue
+			remove_child(loot)
+			area.get_parent().add_child(loot)
 		get_tree().queue_delete(self)
